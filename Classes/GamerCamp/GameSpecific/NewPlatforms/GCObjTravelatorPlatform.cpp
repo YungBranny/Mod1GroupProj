9f5ba968de1af1a6f7eb8b8ffd18706a39309f06
YungBranny <52447656+YungBranny@users.noreply.github.com>
2020-11-10 13:55:43 +0000

    moving platforms

--- Classes/GamerCamp/GameSpecific/NewPlatforms/GCObjTravelatorPlatform.cpp
+++ Classes/GamerCamp/GameSpecific/NewPlatforms/GCObjTravelatorPlatform.cpp
@@ -48,51 +48,51 @@ CGCObjTravelatorPlatform::CGCObjTravelatorPlatform()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //IN_CPP_CREATION_PARAMS_DECLARE(CGCObjTravelatorPlatform, "TexturePacker/Sprites/LongPlatformTest/LongPlatformTest.plist", "LongPlatformTest", b2_staticBody, true); //file path and physics body for the travelator
 
-void CGCObjTravelatorPlatform::VOnResourceAcquire()
-{
+//void CGCObjTravelatorPlatform::VOnResourceAcquire()
+//{
+//
+//	//IN_CPP_CREATION_PARAMS_AT_TOP_OF_VONRESOURCEACQUIRE(CGCObjTravelatorPlatform);
+//
+//	CGCObjSpritePhysics::VOnResourceAcquire();
+//	
+//	//SetResetPosition(GetStartPos()); //Start position
+//	
+//	//cocos2d::ValueMap dicPList = GCCocosHelpers::CreateDictionaryFromPlist(GetFactoryCreationParams()->strPlistFile);
+//
+//}
 
-	//IN_CPP_CREATION_PARAMS_AT_TOP_OF_VONRESOURCEACQUIRE(CGCObjTravelatorPlatform);
 
-	CGCObjSpritePhysics::VOnResourceAcquire();
-	
-	SetResetPosition(GetStartPos()); //Start position
-	
-	cocos2d::ValueMap dicPList = GCCocosHelpers::CreateDictionaryFromPlist(GetFactoryCreationParams()->strPlistFile);
-
-}
-
-
-
-void CGCObjTravelatorPlatform::VOnReset()
-{
-	CGCObjSpritePhysics::VOnReset();//Reset
-	
-	SetResetPosition(GetStartPos()); //start position
-	
-	if (GetPhysicsBody())
-	{
-		cocos2d::Vec2 v2SpritePos = GetSpritePosition(); //gets sprite current position
-		
-		GetPhysicsBody()->SetTransform(IGCGameLayer::B2dPixelsToWorld(b2Vec2(v2SpritePos.x, v2SpritePos.y)), 0.0f); //set position of the sprite
-		
-		GetPhysicsBody()->SetFixedRotation(true);
-	}
-
-}
+//
+//void CGCObjTravelatorPlatform::VOnReset()
+//{
+//	CGCObjSpritePhysics::VOnReset();//Reset
+//	
+//	//SetResetPosition(GetStartPos()); //start position
+//	
+//	if (GetPhysicsBody())
+//	{
+//		cocos2d::Vec2 v2SpritePos = GetSpritePosition(); //gets sprite current position
+//		
+//		GetPhysicsBody()->SetTransform(IGCGameLayer::B2dPixelsToWorld(b2Vec2(v2SpritePos.x, v2SpritePos.y)), 0.0f); //set position of the sprite
+//		
+//		GetPhysicsBody()->SetFixedRotation(true);
+//	}
+//
+//}
 void CGCObjTravelatorPlatform::VOnUpdate(f32 fTimestep)
 {
 }
-
-void CGCObjTravelatorPlatform::VOnResourceRelease()
-{
-	CGCObjSpritePhysics::VOnResourceRelease();
-
-}
-void CGCObjTravelatorPlatform::VOnResurrected()
-{
-	CGCObjSpritePhysics::VOnResurrected();
-
-}
+//
+//void CGCObjTravelatorPlatform::VOnResourceRelease()
+//{
+//	CGCObjSpritePhysics::VOnResourceRelease();
+//
+//}
+//void CGCObjTravelatorPlatform::VOnResurrected()
+//{
+//	CGCObjSpritePhysics::VOnResurrected();
+//
+//}
 
 
 
--- Classes/GamerCamp/GameSpecific/NewPlatforms/GCObjTravelatorPlatform.h
+++ Classes/GamerCamp/GameSpecific/NewPlatforms/GCObjTravelatorPlatform.h
@@ -4,7 +4,7 @@
 
 #include "GamerCamp/GCCocosInterface/GCObjSpritePhysics.h"
 
-#include "GamerCamp/GCCocosInterface/GB2ShapeCache-x.h"
+
 #include "GamerCamp/GCCocosInterface/GCFactory_ObjSpritePhysics.h"
 
 
@@ -13,7 +13,7 @@ class CGCObjTravelatorPlatform
 	: public CGCObjSpritePhysics
 {
 private:
-	cocos2d::Vec2 m_v2StartPos;
+	//cocos2d::Vec2 m_v2StartPos;
 
 	cocos2d::Vec2 m_v2Velocity;
 
@@ -26,14 +26,14 @@ public:
 	
 	/////////////////////////////////////////////////////////////////////////////
 	
-	cocos2d::Vec2 GetStartPos() { return m_v2StartPos; }
-	void SetStartPos(cocos2d::Vec2 i) { m_v2StartPos = i; }
+	//cocos2d::Vec2 GetStartPos() { return m_v2StartPos; }
+	//void SetStartPos(cocos2d::Vec2 i) { m_v2StartPos = i; }
 
 	/////////////////////////////////////////////////////////////////////////////
-	virtual void VOnResourceAcquire(void);
-	virtual void VOnResurrected(void);
-	virtual void VOnResourceRelease(void);
-	virtual void VOnReset(void);
+	//virtual void VOnResourceAcquire(void);
+	//virtual void VOnResurrected(void);
+	//virtual void VOnResourceRelease(void);
+	//virtual void VOnReset(void);
 	virtual void VOnUpdate(f32 fTimestep);
 	
 	cocos2d::Vec2 getVelocity() const { return m_v2Velocity; }
