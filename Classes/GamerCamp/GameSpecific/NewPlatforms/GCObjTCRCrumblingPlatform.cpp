5fe2d8d09965ddbf08914aed33afaeebdc1c09a3
YungBranny <Middleton.Brandon1997@gmail.com>
2020-12-09 18:45:55 +0000

    Made plist and classes for crumbling platform animations

---
+++ Classes/GamerCamp/GameSpecific/NewPlatforms/GCObjTCRCrumblingPlatform.cpp
@@ -0,0 +1,124 @@
+#include <memory.h>
+
+//#include "AppDelegate.h"
+#include "GamerCamp/GCCocosInterface/GCCocosHelpers.h"
+#include "GamerCamp/GCObject/GCObjectManager.h"
+#include "GamerCamp/GameSpecific/GCGameLayerPlatformer.h"
+
+#include "GCObjTCRCrumblingPlatform.h"
+
+#include "../../GCCocosInterface/GB2ShapeCache-x.h"
+
+GCFACTORY_IMPLEMENT_CREATEABLECLASS (CGCObjTCRCrumblingPlatform);
+
+
+CGCObjTCRCrumblingPlatform::CGCObjTCRCrumblingPlatform ()
+	: CGCObjSpritePhysics (GetGCTypeIDOf (CGCObjScalingFallingPlatform))
+	, m_v2FallingVelocity (0.0f, -1.0f)									  //Initalises the variable with a default value
+	, m_v2DefaultVelocity (0.0f, 0.0f)									  //Initalises the variable with a default value
+	, m_bContactWithPlayer (false)										  //Initalises the variable with a default value
+	, m_fMaxDestroyPlatformTick (60.0f)
+	, m_fCurrentDestroyPlatformTick (m_fMaxDestroyPlatformTick)
+	, m_bCanDelete (false)		//Initalises the variable with a default value
+
+{
+}
+
+//Sets the sprite
+//IN_CPP_CREATION_PARAMS_DECLARE (CGCObjScalingFallingPlatform, "TexturePacker/Sprites/FallingScalingPlatform/FallingScalingPlatform.plist", "FallingScalingPlatform", b2_kinematicBody, true);
+
+void CGCObjTCRCrumblingPlatform::VOnResourceAcquire ()
+{
+
+	//IN_CPP_CREATION_PARAMS_AT_TOP_OF_VONRESOURCEACQUIRE (CGCObjScalingFallingPlatform);
+
+	CGCObjSpritePhysics::VOnResourceAcquire ();
+
+	CGCObjSpritePhysics::VOnResourceAcquire ();
+	//const char* pszPlist_SeaUrchin = "TexturePacker/Sprites/SeaUrchin/SeaUrchin.plist";
+	const char* pszAnim_Idle = "TCRIdle";
+
+	cocos2d::ValueMap dicPList = GCCocosHelpers::CreateDictionaryFromPlist (GetFactoryCreationParams ()->strPlistFile);
+	RunAction (GCCocosHelpers::CreateAnimationActionLoop (GCCocosHelpers::CreateAnimation (dicPList, pszAnim_Idle)));
+
+
+
+
+}
+void CGCObjTCRCrumblingPlatform::VOnReset ()
+{
+	CGCObjSpritePhysics::VOnReset ();
+	m_bContactWithPlayer = false;
+	m_bCanDelete = false;
+	m_fCurrentDestroyPlatformTick = m_fMaxDestroyPlatformTick;
+
+	if (GetPhysicsBody ())
+	{
+		cocos2d::Vec2 v2SpritePos = GetSpritePosition ();
+		GetPhysicsBody ()->SetLinearVelocity (b2Vec2 (0.0f, 0.0f));
+		GetPhysicsBody ()->SetTransform (IGCGameLayer::B2dPixelsToWorld (b2Vec2 (v2SpritePos.x, v2SpritePos.y)), 0.0f);
+		GetPhysicsBody ()->SetFixedRotation (true);
+		m_v2CurrentPos = GetPhysicsBody ()->GetPosition ();
+	}
+}
+
+//Calls the function that moves the platform down
+void CGCObjTCRCrumblingPlatform::MoveDownOnContact ()
+{
+	//if this boolean gets set to true it starts decreasing the platform tick 
+	//and changes the velocity of the platform so it moves down
+	if (GetContactWithPlayer () == true)
+	{
+		//SetVelocity (m_v2FallingVelocity);
+
+		if (m_fCurrentDestroyPlatformTick >= 0)
+		{
+			m_fCurrentDestroyPlatformTick--;
+			SetSpritePosition (cocos2d::Vec2 (GetSpritePosition ().x, GetSpritePosition ().y - 3.0f));
+		}
+
+		if (m_fCurrentDestroyPlatformTick <= 0)
+		{
+			m_bCanDelete = true;
+		}
+	}
+	else  // changes the velocity to 0 so it stops moving
+	{
+		//SetVelocity (m_v2DefaultVelocity);
+	}
+}
+
+void CGCObjTCRCrumblingPlatform::VHandleFactoryParams (const CGCFactoryCreationParams& rCreationParams, cocos2d::Vec2 v2InitialPosition)
+{
+	const CGCFactoryCreationParams* pParamsToPassToBaseClass = &rCreationParams;
+
+	if (nullptr != CGCLevelLoader_Ogmo::sm_pCurrentObjectXmlData)
+	{
+		//const tinyxml2::XMLAttribute* pName = CGCLevelLoader_Ogmo::sm_pCurrentObjectXmlData->FindAttribute( "name" );
+
+		//CCLOG( (nullptr == pName) ? "BOB NOT FOUND!" : pName->Value() );
+
+		const tinyxml2::XMLAttribute* pCustomPlistPath = CGCLevelLoader_Ogmo::sm_pCurrentObjectXmlData->FindAttribute ("PlistFile");    //customplist    //PlistFile
+
+		const tinyxml2::XMLAttribute* pCustomShape = CGCLevelLoader_Ogmo::sm_pCurrentObjectXmlData->FindAttribute ("shape");
+
+		if (( nullptr != pCustomPlistPath ) && ( 0 != strlen (pCustomPlistPath->Value ()) ))    // && ( (nullptr != pCustomShape) && ( 0 != strlen( pCustomShape->Value() ) ) ) )
+		{
+			m_pCustomCreationParams = std::make_unique< CGCFactoryCreationParams > (rCreationParams.strClassName.c_str (),
+				pCustomPlistPath->Value (),
+				pCustomShape->Value (),
+				rCreationParams.eB2dBody_BodyType,
+				rCreationParams.bB2dBody_FixedRotation);
+
+			pParamsToPassToBaseClass = m_pCustomCreationParams.get ();
+		}
+	}
+
+	CGCObjSpritePhysics::VHandleFactoryParams (( *pParamsToPassToBaseClass ), v2InitialPosition);
+}
+//update function
+void CGCObjTCRCrumblingPlatform::VOnUpdate (f32 fTimestep)
+{
+	MoveDownOnContact ();
+}
+
---
+++ Classes/GamerCamp/GameSpecific/NewPlatforms/GCObjTCRCrumblingPlatform.h
@@ -0,0 +1,69 @@
+#ifndef _CGCOBJTCRCRUMBLINGPLATFORM_
+#define _CGCOBJTCRCRUMBLINGPLATFORM__
+
+
+
+//Brandon Middleton
+//This class draws a single platform when falls when it is stood on, this is intended to be used in the platform manager so it can be scaled
+//Refactoring needs to be done so the platform doesnt move down but just deletes after a while
+
+#include "GamerCamp/GCCocosInterface/GCObjSpritePhysics.h"
+#include "../../GCCocosInterface/GCFactory_ObjSpritePhysics.h"
+
+class CGCObjTCRCrumblingPlatform
+	: public CGCObjSpritePhysics
+{
+private:
+	cocos2d::Vec2 m_v2FallingVelocity;	//vector2 for the Falling velocity/speed which is no longer needed
+
+	cocos2d::Vec2 m_v2DefaultVelocity;	//vector2 for the Velocity which is no longer needed
+
+	b2Vec2 m_v2CurrentPos;
+
+	bool m_bContactWithPlayer;			//Bool which needs to be set to true if the player is colliding with this platform
+	bool m_bCanDelete;					//Bool that needs to be set to true once the destroyplatform tick has reached 0 which will then allow the platform to be delete
+
+
+	float m_fCurrentDestroyPlatformTick;		// A float that counts down to 0, once it has reached 0 it will set the can delete bool to true
+	float m_fMaxDestroyPlatformTick;
+	std::unique_ptr< CGCFactoryCreationParams > m_pCustomCreationParams;
+
+
+
+public:
+
+	CGCObjTCRCrumblingPlatform ();												//constructor
+	virtual ~CGCObjTCRCrumblingPlatform () {};									//deconstructor
+
+	GCFACTORY_DECLARE_CREATABLECLASS (CGCObjTCRCrumblingPlatform);
+
+	b2Vec2 getCurrentPos () const { return m_v2CurrentPos; }
+
+	float GetCurrentDestroyPlatformTick () const { return m_fCurrentDestroyPlatformTick; }
+	void  SetCurrentDestroyPlatformTick (float i) { m_fCurrentDestroyPlatformTick = i; }
+
+	float GetMaxDestroyPlatformTick () const { return m_fMaxDestroyPlatformTick; }
+	void  SetMaxDestroyPlatformTick (float i) { m_fMaxDestroyPlatformTick = i; }
+
+	cocos2d::Vec2 GetFallingVelocity () const { return m_v2FallingVelocity; }		//getter for the Current velocity
+	void SetFallingVelocity (cocos2d::Vec2 i) { m_v2FallingVelocity = i; }			//setter for the Current velocity
+
+	cocos2d::Vec2 GetDefaultVelocity () const { return m_v2DefaultVelocity; }		//getter for the Default Velocity
+	void SetDefaultVelocity (cocos2d::Vec2 i) { m_v2DefaultVelocity = i; }			//setter for the Default Velocity
+
+	bool GetContactWithPlayer () const { return m_bContactWithPlayer; }				//getter for the Contact with player bool
+	void SetContactWithPlayer (bool i) { m_bContactWithPlayer = i; }				//setter for the Contact with player bool
+
+	bool GetCanDelete () const { return m_bCanDelete; }								//getter for the Can delete float
+	void SetCanDelete (bool i) { m_bCanDelete = i; }								//setter for the Can delete float
+
+
+	virtual void VOnResourceAcquire (void);											//Default Function
+	virtual void VOnReset (void);
+	void MoveDownOnContact ();														//Function that moved the platform down on contact, no longer needed but will be refactored
+	virtual void VOnUpdate (f32 fTimestep);											//UpdateFunction
+
+	virtual void VHandleFactoryParams (const CGCFactoryCreationParams& rCreationParams, cocos2d::Vec2 v2InitialPosition);
+};
+
+#endif
